c0347f1308c32a421295bca54af5279b1456eae1
lulnetto <lourival.netto@ccc.ufcg.edu.br>
2022-10-23 18:16:24 -0300

    dir

---
+++ lourival_netto/payment_calc/pcp/funcionario.py
@@ -0,0 +1,7 @@
+class funcionario:
+    #Constructor
+    def __init__(self, nome, email, salario_base, cargo):
+        self.nome = nome
+        self.email = email
+        self.salario_base = salario_base
+        self.cargo = cargo
