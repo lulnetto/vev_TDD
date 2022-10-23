d098282dccd40488f72409f619c81c6b709d7bd7
GitBerg <beguinhoboy@hotmail.com>
2022-10-23 19:56:34 -0300

    Escrevendo o teste para criar uma fatura

--- lourival_netto/FiltroDeFaturas/src/test/java/org/example/service/FiltroDeFaturasServiceTest.java
+++ lourival_netto/FiltroDeFaturas/src/test/java/org/example/service/FiltroDeFaturasServiceTest.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 import org.example.models.Cliente;
+import org.example.models.Fatura;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,14 +13,23 @@ class FiltroDeFaturasServiceTest {
     public void create_new_client(){
         //Criar um novo cliente
 
-        Cliente cliente = new Cliente("Berg", "23/10/2022", "Paraíba");
+        Cliente cliente = new Cliente("Berg", "23/3/2022", "Paraíba");
 
         assertEquals("Berg", cliente.getNome());
         assertEquals("23/10/2022", cliente.getData());
         assertEquals("Paraíba", cliente.getEstado());
     }
-    
 
+    public void create_new_invoice(){
+        //Criar uma nova fatura
+
+        Fatura fatura = new Fatura("CODTEST", 100, "20/08/2022", "Berg");
+
+        assertEquals("CODETEST", fatura.getCodigo());
+        assertEquals(100, fatura.getValor());
+        assertEquals("20/08/2022", fatura.getData());
+        assertEquals("Berg", fatura.getCliente());
+    }
 
 
 
