78fdad84089c197bcf2525a8caa22bc53adee604
GitBerg <beguinhoboy@hotmail.com>
2022-10-23 19:25:47 -0300

    Escrevendo o primeiro teste para criar um cliente

---
+++ lourival_netto/FiltroDeFaturas/src/test/java/org/example/service/FiltroDeFaturasServiceTest.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FiltroDeFaturasServiceTest {
+
+    Client client = new Client();
+    client.setName("Berg");
+    client.setDate("23/10/2022");
+    client.setState("Paraíba");
+
+}
