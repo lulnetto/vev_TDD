a1bb8177fd92a828f1777d431092f8bf0de0c65d
GitBerg <beguinhoboy@hotmail.com>
2022-10-23 19:51:26 -0300

    Criando a classe cliente

--- lourival_netto/FiltroDeFaturas/src/main/java/org/example/models/Cliente.java
+++ lourival_netto/FiltroDeFaturas/src/main/java/org/example/models/Cliente.java
@@ -1,4 +1,35 @@
 package org.example.models;
 
+import java.util.UUID;
+
 public class Cliente {
+
+    private UUID id;
+    private String nome;
+    private String data;
+    private String estado;
+
+    public Cliente(String nome, String data, String estado) {
+        this.id = UUID.randomUUID();
+        this.nome = nome;
+        this.data = data;
+        this.estado = estado;
+    }
+
+    public UUID getId(){
+        return this.id;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getEstado(){
+        return this.estado;
+    }
+
+    public String getData(){
+        return this.data;
+    }
+
 }
--- lourival_netto/FiltroDeFaturas/src/test/java/org/example/service/FiltroDeFaturasServiceTest.java
+++ lourival_netto/FiltroDeFaturas/src/test/java/org/example/service/FiltroDeFaturasServiceTest.java
@@ -1,12 +1,26 @@
 package org.example.service;
 
+import org.example.models.Cliente;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class FiltroDeFaturasServiceTest {
 
-    Client client = new Client();
-    client.setName("Berg");
-    client.setDate("23/10/2022");
-    client.setState("Paraíba");
+    @Test
+    public void create_new_client(){
+        //Criar um novo cliente
+
+        Cliente cliente = new Cliente("Berg", "23/10/2022", "Paraíba");
+
+        assertEquals("Berg", cliente.getNome());
+        assertEquals("23/10/2022", cliente.getData());
+        assertEquals("Paraíba", cliente.getEstado());
+    }
+    
+
+
+
 
 }
