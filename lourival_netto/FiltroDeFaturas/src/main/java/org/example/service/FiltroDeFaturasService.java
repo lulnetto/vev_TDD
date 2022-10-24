c920410fd0f6c98df3360011907c0f2b0e44e0b2
GitBerg <beguinhoboy@hotmail.com>
2022-10-23 22:01:13 -0300

    Criando uma função filtro que permite inserção de um array de faturas

--- lourival_netto/FiltroDeFaturas/src/main/java/org/example/service/FiltroDeFaturasService.java
+++ lourival_netto/FiltroDeFaturas/src/main/java/org/example/service/FiltroDeFaturasService.java
@@ -1,4 +1,16 @@
 package org.example.service;
 
+import org.example.models.Fatura;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FiltroDeFaturasService {
+
+    public List<Fatura> filter(Fatura[] faturas){
+        List<Fatura> filtered = new ArrayList<>(Arrays.asList(faturas));
+
+        return filtered;
+    }
 }
